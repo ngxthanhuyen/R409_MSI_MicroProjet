7286ce32504dcd4684bb01e2f4b2719285a39185
LAM-Harry <harrylam317@gmail.com>
2025-05-30 11:11:24 +0200

    user_exists()

--- Partie1/Etape2/modification_script.py
+++ Partie1/Etape2/modification_script.py
@@ -12,3 +12,12 @@ uid = common.authenticate(db, username, password, {})
 
 # Accès aux modèles
 models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object')
+
+# Vérifie si un utilisateur existe
+def user_exists(login):
+    user_ids = models.execute_kw(db, uid, password, 'res.users', 'search', [[('login', '=', login)]])
+    if user_ids:
+        return user_ids[0]
+    else:
+        print(f"Utilisateur avec login '{login}' non trouvé.")
+        return None
