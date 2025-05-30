370b9380d5721dbcbae3f3f97c65b35e7c8e0887
Uyen <nthanhuyen1411@gmail.com>
2025-05-30 18:18:41 +0900

    Fonction update_user_info()

--- Partie1/Etape2/modification_script.py
+++ Partie1/Etape2/modification_script.py
@@ -21,3 +21,18 @@ def user_exists(login):
     else:
         print(f"Utilisateur avec login '{login}' non trouvé.")
         return None
+    
+# Met à jour les informations de l'utilisateur
+def update_user_info(user_id, new_email=None, new_password=None):
+    values = {}
+    if new_email:
+        values['email'] = new_email
+    if new_password:
+        values['password'] = new_password
+
+    if values:
+        success = models.execute_kw(db, uid, password, 'res.users', 'write', [[user_id], values])
+        if success:
+            print(f"Informations de l'utilisateur {user_id} mises à jour.")
+        else:
+            print("Échec de la mise à jour.")
